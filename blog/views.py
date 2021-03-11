7a38d38cd83f177331f16ffa869ce89ee28c68b9
kacpermisiek <315560@uwr.edu.pl>
2021-03-11 18:45:59 +0100

    Podstawowy wygląd strony
    Przygotowanie podstawowego wyglądu strony:
    navbar, miejsce na posty(dodałem ręcznie kilka dla wyglądu), sidebar

--- blog/views.py
+++ blog/views.py
@@ -2,9 +2,44 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+posts = [
+    {
+        'author': 'Kacper',
+        'title': 'Blog Post 1',
+        'content': 'Pierwszy post testowy',
+        'date_posted': '21 marca 2021'
+    },
+
+    {
+        'author': 'Kacper2',
+        'title': 'Blog Post 2',
+        'content': 'Drugi post testowy',
+        'date_posted': '22 marca 2021'
+    },
+
+    {
+        'author': 'Kacper3',
+        'title': 'Blog Post 3',
+        'content': 'Trzeci post testowy',
+        'date_posted': '21 marca 2021'
+    },
+
+    {
+        'author': 'Kacper4',
+        'title': 'Blog Post 4',
+        'content': 'Czwarty',
+        'date_posted': '21 marca 2021'
+    },
+
+]
+
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title' : 'About'})
