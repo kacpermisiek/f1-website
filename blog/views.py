b0fe7139e0dcc943c3a285d78b8f5b9d4f669174
kacpermisiek <315560@uwr.edu.pl>
2021-03-19 10:47:49 +0100

    Taski na marzec skończone
    *Stworzenie superusera, moderatora oraz użytkownika,
    
    *Stworzenie bazy danych postów (autor, tytuł, treść, data)

--- blog/views.py
+++ blog/views.py
@@ -1,42 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-posts = [
-    {
-        'author': 'Kacper',
-        'title': 'Blog Post 1',
-        'content': 'Pierwszy post testowy',
-        'date_posted': '21 marca 2021'
-    },
-
-    {
-        'author': 'Kacper2',
-        'title': 'Blog Post 2',
-        'content': 'Drugi post testowy',
-        'date_posted': '22 marca 2021'
-    },
-
-    {
-        'author': 'Kacper3',
-        'title': 'Blog Post 3',
-        'content': 'Trzeci post testowy',
-        'date_posted': '21 marca 2021'
-    },
-
-    {
-        'author': 'Kacper4',
-        'title': 'Blog Post 4',
-        'content': 'Czwarty',
-        'date_posted': '21 marca 2021'
-    },
-
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
