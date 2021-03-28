d82f9011d679c5b3bafc07132640a5835cfe8778
kacpermisiek <315560@uwr.edu.pl>
2021-03-28 15:11:42 +0200

    Zmiana wyglądu formularza
    Dodano Crispy form

--- django_project/settings.py
+++ django_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
     'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
