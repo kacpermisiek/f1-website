6504b56ee357261dd2b494fb32d7b683d1dd0ee9
kacpermisiek <315560@uwr.edu.pl>
2021-04-06 14:38:56 +0200

    Profil użytkownika, zdjęcie użytkownika
    Użytkownik ma stronę "mój profil",
    możliwe jest ustawienie zdjęcia użytkownika (jak na razie tylko ręcznie)

--- users/views.py
+++ users/views.py
@@ -19,6 +19,7 @@ def register(request):
 
     return render(request, 'users/register.html', {'form': form})
 
+
 @login_required
 def profile(request):
     return render(request, 'users/profile.html')
