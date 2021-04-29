70fde1dda44691b0f2f745b8f8b178ee7d24c5ea
kacpermisiek <315560@uwr.edu.pl>
2021-04-29 12:06:30 +0200

    Zmiana nazwy apki stats -> races

--- stats/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-def stats_drivers(request):
-    return render(request, 'stats/stats_drivers.html', {'title': 'F1 Blog Drivers Stats'})
