d7a137abdb0853b48a09ed416e1c6b53e79974a0
kacpermisiek <315560@uwr.edu.pl>
2021-05-16 11:23:49 +0200

    Przyciski do tworzenia, edytowania i usuwania postów

--- blog/models.py
+++ blog/models.py
@@ -7,8 +7,9 @@ from django.urls import reverse
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(default=timezone.localtime())
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # image = models.ImageField(upload_to='images')
 
     def __str__(self):
         return self.title
@@ -16,4 +17,4 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post-detail', kwargs={'pk': self.pk})
 
-    # image = models.ImageField(upload_to='images')
+
