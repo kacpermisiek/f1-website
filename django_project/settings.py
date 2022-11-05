4127c7dc2ff23fd8d8480f27bc2f91877afc8372
kacpermisiek <56176866+kacpermisiek@users.noreply.github.com>
2022-11-05 12:04:41 +0100

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -139,4 +139,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'issp2021website@gmail.com'
-EMAIL_HOST_PASSWORD = 'jemvilqisnauhdvj'
+EMAIL_HOST_PASSWORD = $HOST_PSWD
